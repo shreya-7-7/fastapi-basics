98da2ac0e09e4c828a2d6976ca785e5e86627975
shreya-7-7 <shreya125566@gmail.com>
2026-06-29 12:22:52 +0530

    Refactor: Update database implementation to use SQLAlchemy

--- 13_Sql_lite.py
+++ 13_Sql_lite.py
@@ -5,17 +5,22 @@ from fastapi import FastAPI,Depends
 
 app = FastAPI()
 
+#Database URL
 DATABASE_URL = "sqlite:///./test.db"
 
+# Engine create (DB connection)
 engine = create_engine(
     DATABASE_URL,
     connect_args={"check_same_thread":False}
 )
 
+# Session (For DB Operations)
 sessionLocal = sessionmaker(bind=engine)
 
+# Base (For Base Model)
 Base = declarative_base()
 
+# Table (Model)
 class Todo(Base):
     __tablename__ = "todos"
 
@@ -23,8 +28,10 @@ class Todo(Base):
     title = Column(String)
     completed = Column(String)
 
+# Table create
 Base.metadata.create_all(bind=engine)
 
+# Dependency (It will provide DB session)
 def get_db():
     db = sessionLocal()
     try:
@@ -32,8 +39,21 @@ def get_db():
     finally:
         db.close()
 
-@app.get("/")
-def home(db: Session = Depends(get_db)):
+# @app.get("/")
+# def home(db: Session = Depends(get_db)):
+#     return {
+#         "message":"DB connected Fine"
+#     }
+
+
+# Create API (Using POST)
+@app.post("/todos")
+def create_todo(title:str, db: Session = Depends(get_db)):
+    todo = Todo(title=title, completed = "False") #object created (initially value is false if there is nothing) 
+    db.add(todo) #add to DB
+    db.commit() #save to DB
+    db.refresh(todo) #to get id of latest data
     return {
-        "message":"DB connected Fine"
+        "message":"Todo Created",
+        "data":todo
     }
